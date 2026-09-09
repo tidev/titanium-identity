93d55d741226f5df5fc287b901e4958b69c6d3d9
m1ga <migamiga@web.de>
2026-09-09 20:38:28 +0200

    fix linting

--- ios/Classes/TiIdentityModuleAssets.m
+++ ios/Classes/TiIdentityModuleAssets.m
@@ -3,20 +3,18 @@
  */
 #import "TiIdentityModuleAssets.h"
 
-extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+extern NSData *filterDataInRange(NSData *thedata, NSRange range);
 
 @implementation TiIdentityModuleAssets
 
 - (NSData *)moduleAsset
 {
-  
 
   return nil;
 }
 
 - (NSData *)resolveModuleAsset:(NSString *)path
 {
-  
 
   return nil;
 }
